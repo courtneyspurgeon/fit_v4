68bb8e39663a759b858c1254cedb1d9b491f13c9
Courtney Spurgeon <courtneyspurgeon@mac-courtneys.local>
2013-10-03 11:02:05 -0700

    attempt with rails 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FivV4::Application.config.secret_key_base = 'e1f6b673271fdd0dde999ed9f97aac6617f45a1b1380ca5b0e8821c38f42ba412855651cc37db440464757b9693548c60407aab9adf076790e22197a2d9ece0d'
