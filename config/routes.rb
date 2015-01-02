f5e7036dfd6bf352512caee6f802eb19bcee69e2
Courtney Spurgeon <courtney.spurgeon@smashingideas.com>
2015-01-01 22:32:42 -0800

    Configuring devise for users instead of members, adding username and validation for username

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 FivV4::Application.routes.draw do
   
 
+  devise_for :users
   resources :brands
 
   resources :pants do
@@ -8,7 +9,7 @@ FivV4::Application.routes.draw do
   end
   resources :reviews
 
-  devise_for :members
+  # devise_for :members
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131011211931) do
+ActiveRecord::Schema.define(version: 20150102055401) do
 
   create_table "attachments", force: true do |t|
     t.integer  "attachable_id"
@@ -105,5 +105,28 @@ ActiveRecord::Schema.define(version: 20131011211931) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "username"
+    t.integer  "waist_measurement"
+    t.integer  "hip_measurement"
+    t.integer  "thigh_measurement"
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
