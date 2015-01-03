6c07bd61dda25842f3745d2fda7eb2758e878e14
Courtney Spurgeon <spurgeon.cj@gmail.com>
2015-01-03 13:28:45 -0800

    [Devise] Removing custom route info for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 FivV4::Application.routes.draw do
   
 
-  # devise_for :users
-  devise_for :users, :controllers => { :registrations => :registrations }
+  devise_for :users
+  # devise_for :users, :controllers => { :registrations => :registrations }
 
   resources :brands
 
